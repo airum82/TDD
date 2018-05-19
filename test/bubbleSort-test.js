e069ec22b6fb47eabd9007dab9a9735826d4bf88
Arram Mandel <airum82@gmail.com>
2018-05-18 20:14:32 -0600

    Added additional tests to insertion and bubble tests

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -2,6 +2,9 @@ const sortArray = require('../lib/bubbleSort.js').sortArray;
 import { assert } from 'chai';
 
 describe('test', function() {
+    it('It should exist as a function', () => {
+      assert.isFunction(sortArray);
+    })
     it('should sort an unsorted array from lowest to highest using bubble method', function() {
       let unsortedArray = [6, 3, 9, 2, 5];
       sortArray(unsortedArray);
@@ -11,5 +14,9 @@ describe('test', function() {
       let unsortedArray = ['cat', 'dog', 'snake', 'rat'];
       sortArray(unsortedArray);
       assert.deepEqual(unsortedArray, ['cat', 'dog', 'rat', 'snake']);
+    })
+    it('should work with a larger array', () => {
+      let unsortedArray = [5, 7, 9, 3, 10, 4, 6, 3, 6, 8, 9, 11];
+      assert.deepEqual(sortArray(unsortedArray), [3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10, 11]);
     })
   });
--- test/insertionSort-test.js
+++ test/insertionSort-test.js
@@ -2,6 +2,9 @@ const insertionSortArray = require('../lib/insertionSort.js').insertionSortArray
 import { assert } from 'chai';
 
 describe('test', function() {
+  it('should exist as a function', () => {
+    assert.isFunction(insertionSortArray);
+  })
   it('should sort an array of numbers from highest to lowest', function() {
     let unsortedArray = [6, 3, 9, 2, 5, 7, 10];
     assert.deepEqual(insertionSortArray(unsortedArray), [10, 9, 7, 6, 5, 3, 2]);
@@ -9,5 +12,9 @@ describe('test', function() {
   it('should sort an array of words as well', () => {
     let animalArray = ['bear', 'fox', 'deer', 'wolverine'];
     assert.deepEqual(insertionSortArray(animalArray), ['wolverine', 'fox', 'deer', 'bear']);
+  })
+  it('should work with a larger array', () => {
+    let unsortedArray = [5, 7, 9, 3, 10, 4, 6, 3, 6, 8, 9, 11];
+    assert.deepEqual(insertionSortArray(unsortedArray), [11, 10, 9, 9, 8, 7, 6, 6, 5, 4, 3, 3]);
   })
 })
