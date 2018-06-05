f4439ea591a1d26c8206f25730128c7bd0dd7a58
Arram Mandel <airum82@gmail.com>
2018-06-05 15:11:10 -0600

    Created large array test for all alalgorithms

--- test/insertionSort-test.js
+++ test/insertionSort-test.js
@@ -17,4 +17,44 @@ describe('test', function() {
     let unsortedArray = [5, 7, 9, 3, 10, 4, 6, 3, 6, 8, 9, 11];
     assert.deepEqual(insertionSortArray(unsortedArray), [11, 10, 9, 9, 8, 7, 6, 6, 5, 4, 3, 3]);
   })
+  it('should work on a very large array of larger numbers', () => {
+    let superBigArray = [45, 65, 45, 70, 75, 80, 100, 3, 5, 7, 10, 11, 12, 13, 14, 10, 120, 140, 155, 160, 170, 180, 200, 220, 240, 250, 69, 95, 430, 350, 100, 90, 35, 20, 980, 67, 20];
+    assert.deepEqual(insertionSortArray(superBigArray), [ 980,
+  430,
+  350,
+  250,
+  240,
+  220,
+  200,
+  180,
+  170,
+  160,
+  155,
+  140,
+  120,
+  100,
+  100,
+  95,
+  90,
+  80,
+  75,
+  70,
+  69,
+  67,
+  65,
+  45,
+  45,
+  35,
+  20,
+  20,
+  14,
+  13,
+  12,
+  11,
+  10,
+  10,
+  7,
+  5,
+  3 ])
 })
+});
--- test/quickSort-test.js
+++ test/quickSort-test.js
@@ -1,25 +1,25 @@
-const mergeSortArray = require('../lib/mergeSort.js').mergeSort;
 import { assert } from 'chai';
+const quickSort = require("../lib/quickSort.js").quickSort;
 
-describe('test', function() {
-  it('should exist as a function', () => {
-    assert.isFunction(mergeSortArray);
+describe.only('test', function() {
+   it('should exist as a function', () => {
+    assert.isFunction(quickSort);
   })
-  it('should merge sort an array of numbers from lowest to highest', function() {
+  it('should quick sort an array of numbers from lowest to highest', function() {
     let unsortedArray = [6, 3, 9, 5]
-    assert.deepEqual(mergeSortArray(unsortedArray), [3, 5, 6, 9])
+    assert.deepEqual(quickSort(unsortedArray), [3, 5, 6, 9])
   })
   it('should sort an array of words as well', () => {
     let animalArray = ['bear', 'fox', 'deer', 'wolverine'];
-    assert.deepEqual(mergeSortArray(animalArray), ['bear', 'deer', 'fox', 'wolverine']);
+    assert.deepEqual(quickSort(animalArray), ['bear', 'deer', 'fox', 'wolverine']);
   })
   it('should work with a larger array', () => {
     let unsortedArray = [5, 7, 9, 3, 10, 4, 6, 3, 6, 8, 9, 11];
-    assert.deepEqual(mergeSortArray(unsortedArray), [ 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10, 11 ]);
+    assert.deepEqual(quickSort(unsortedArray), [ 3, 3, 4, 5, 6, 6, 7, 8, 9, 9, 10, 11 ]);
   })
   it('should work on a very large array of larger numbers', () => {
     let superBigArray = [45, 65, 45, 70, 75, 80, 100, 3, 5, 7, 10, 11, 12, 13, 14, 10, 120, 140, 155, 160, 170, 180, 200, 220, 240, 250, 69, 95, 430, 350, 100, 90, 35, 20, 980, 67, 20];
-    assert.deepEqual(mergeSortArray(superBigArray), [ 3,
+    assert.deepEqual(quickSort(superBigArray), [ 3,
   5,
   7,
   10,
